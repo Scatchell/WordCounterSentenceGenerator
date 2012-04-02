bb332a7ed0a7b4c1ad46b6e76bd171b875575c65
Mark Billie <mbillie1@gmail.com>
2012-04-02 16:21:12 -0500

    added emerson

--- src/SentenceGenerator.java
+++ src/SentenceGenerator.java
@@ -12,23 +12,7 @@ public class SentenceGenerator {
     public void generate() {
         StringBuilder sb = new StringBuilder();
         ArrayList<Word> wordsClone = (ArrayList<Word>) this.repository.getWordList().clone();
-        // randomized starting word to get a better feel for what is happening
         Word word = wordsClone.get((int) (Math.random() * wordsClone.size()));
-//        for (int i = 0; i < numOfWords; i++) {
-//            if (i == numOfWords - 1) {
-//                for (Word next : wordsClone) {
-//                    Integer eosLink = next.getEOSIndexOrNull();
-//                    if (eosLink != null) {
-//                        sb.append(next.word).append(".");
-//                        break;
-//                    }
-//                }
-//            } else {
-//                //todo words.get has been moved to top.  Change so each link is searched for in the words list, and used as the next word.  Also need to remove words from array as they are used so they are not used twice
-//                String link = word.getBestLink().toString();
-//                sb.append(word.word + " " + link + " ");
-//            }
-//        }
         String sentence = buildString(word, numOfWords);
         sb.append(sentence);
         System.out.println(sb.toString());
