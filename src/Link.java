8bc15383c40e69339803ebf264ac736e6a1488a0
Mark Billie <mbillie1@gmail.com>
2012-04-02 17:18:51 -0500

    no longer allows consecutive identical words

--- src/Link.java
+++ src/Link.java
@@ -13,7 +13,7 @@ public class Link {
     }
     
     public void downRate() {
-        this.rating -= (int) (Math.random() * 4);
+        this.rating -= (int) (Math.random() * 20) + 1;
     }
 
     @Override
